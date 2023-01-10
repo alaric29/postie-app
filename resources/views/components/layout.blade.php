a143e60a677779a878eac6cdaab29b3dd069f6d8
alaric29 <alaric919@gmail.com>
2023-01-10 19:48:17 +0100

    add logo and favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>OurApp</title>
+    <title>Postie</title>
+    <link rel="icon" href="{{ URL::asset('favicon.png') }}" type="image/x-icon"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
     <script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -15,7 +16,9 @@
   <body>
     <header class="header-bar mb-3">
       <div class="container d-flex flex-column flex-md-row align-items-center p-3">
-        <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">OurApp</a></h4>
+        <img class="logo" src={{ URL::asset('logo.png') }} />
+        <h4 class="my-0 mr-md-auto font-weight-normal"><a href="/" class="text-white">Postie</a></h4>
+        
         @auth
         <div class="flex-row my-3 my-md-0">
           <a href="#" class="text-white mr-2 header-search-icon" title="Search" data-toggle="tooltip" data-placement="bottom"><i class="fas fa-search"></i></a>
@@ -38,7 +41,7 @@
               <input name="loginpassword" class="form-control form-control-sm input-dark" type="password" placeholder="Password" />
             </div>
             <div class="col-md-auto">
-              <button class="btn btn-primary btn-sm">Sign In</button>
+              <button class="btn btn-dark btn-sm">Sign In</button>
             </div>
           </div>
         </form>
